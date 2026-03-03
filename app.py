7805aa7a72dc64bd527efd4af54f47c361097de2
1abdo222004 <157881653+1abdo222004@users.noreply.github.com>
2026-03-03 06:02:21 +0100

    Update and rename newfile.py to app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from tiktok_uploader.upload import upload_video
 
-upload_video('video.mp4', 
+upload_video('1772341527_watermarked_ce26c880_20260301130525 (1).mp4', 
             description='هذا هو وصف الفيديو', 
             cookies='cookies.txt')
